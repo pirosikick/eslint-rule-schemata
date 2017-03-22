56df2c15a67eb0fa7bf9d3d3b8b95b16455c5c8e
Hiroyuki Anai <pirosikick@gmail.com>
2017-03-22 23:00:38 +0900

    Added 3.18.0 schemata

--- generate.js
+++ generate.js
@@ -1,29 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 const reduce = require('lodash.reduce');
+const forEach = require('lodash.foreach');
 const loadRules = require('eslint/lib/load-rules');
+const baseSchema = require('./schema.json');
 
 const packages = [
   'eslint',
 ];
-const toJSON = data => JSON.stringify(data, null, 2);
+const toPrettyJSON = data => JSON.stringify(data, null, 2);
 
 packages.forEach(package => {
   const version = require(`${package}/package.json`).version;
-  const rules = loadRules(`node_modules/${package}/lib/rules`, __dirname);
-  const definitions = reduce(rules, (defs, filepath, name) => {
-    const rule = require(filepath);
-    const schemata = rule.meta.schema;
-    defs[name] = rule.meta.schema;
-    return defs;
-  }, {});
-
-  const dir = path.join(__dirname, package, version);
+  const dir = path.join(__dirname, package, version, 'rules');
 
   try {
-    fs.mkdirSync(dir);
-  } catch(err) { }
+  fs.mkdirSync(dir);
+  } catch (err) { }
 
-  fs.writeFileSync(path.join(dir, 'schema.json'), toJSON(definitions));
+  const rules = loadRules(`node_modules/${package}/lib/rules`, __dirname);
+
+  forEach(rules, (filepath, name) => {
+    const rule = require(filepath);
+    const schema = rule.meta.schema;
+    fs.writeFileSync(path.join(dir, `${name}.json`), JSON.stringify(schema));
+    fs.writeFileSync(path.join(dir, `${name}-pretty.json`), toPrettyJSON(schema));
+  });
 });
-
